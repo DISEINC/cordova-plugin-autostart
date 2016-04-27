65336dcf321d24f850c543bf5da326001990ddc8
Patrik Lindahl <patrik@dise.com>
2016-04-27 11:44:13 +0200

    Updated the delay to be in all possible startup conditions because it was needed for some specific hardware configurations

--- src/android/PackageReplacedReceiver.java
+++ src/android/PackageReplacedReceiver.java
@@ -4,13 +4,22 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import com.tonikorin.cordova.plugin.autostart.AppStarter;
+import android.os.Handler;
+
  
 public class PackageReplacedReceiver extends BroadcastReceiver {
     
     @Override
-    public void onReceive(Context context, Intent intent) {
-    
-        AppStarter appStarter = new AppStarter();
-        appStarter.run(context, intent, AppStarter.BYPASS_USERPRESENT_MODIFICATION );
+    public void onReceive(final Context context, final Intent intent) {
+		// Wait for hardware to init
+		Handler handler = new Handler();
+		handler.postDelayed(new Runnable() {
+			@Override
+			public void run()
+			{
+				AppStarter appStarter = new AppStarter();
+				appStarter.run(context, intent, AppStarter.BYPASS_USERPRESENT_MODIFICATION );
+			}
+		}, 10 * 1000);
     }
 }
--- src/android/UserPresentReceiver.java
+++ src/android/UserPresentReceiver.java
@@ -5,13 +5,22 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import com.tonikorin.cordova.plugin.autostart.AppStarter;
+import android.os.Handler;
+
  
 public class UserPresentReceiver extends BroadcastReceiver {
     
     @Override
-    public void onReceive(Context context, Intent intent) {
-    
-        AppStarter appStarter = new AppStarter();
-        appStarter.run(context, intent, PackageManager.COMPONENT_ENABLED_STATE_DISABLED);
+    public void onReceive(final Context context, final Intent intent) {
+		// Wait for hardware to init
+		Handler handler = new Handler();
+		handler.postDelayed(new Runnable() {
+			@Override
+			public void run()
+			{
+				AppStarter appStarter = new AppStarter();
+				appStarter.run(context, intent, PackageManager.COMPONENT_ENABLED_STATE_DISABLED);
+			}
+		}, 10 * 1000);
     }
 }
